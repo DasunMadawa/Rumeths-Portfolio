57fe3a5f8be07d34b62687dab06abdfb15b599a5
DasunMadawa <dasunmadawa8864@gmail.com>
2024-02-19 01:34:34 +0530

    add theme btn styles

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// $("#theme").hover( function () {
+//     $("#theme-wrapper").css({
+//         backgroundColor:"black"
+//     });
+// } , function () {
+//     $("#theme-wrapper").css({
+//         backgroundColor: "white"
+//     });
+// });
+
+
